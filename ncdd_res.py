6646ba24627e083b7208227713cca51b8377f326
weijiasu <weijiasu0804@gmail.com>
2023-02-07 14:11:51 -0500

    update Type

--- ncdd_res.py
+++ ncdd_res.py
@@ -10,47 +10,25 @@ pd.set_option("display.max_columns",40)
 res1=sys.argv[1]
 name=sys.argv[2]
 
-def circleType(x):
-	cirCle_S=int(x.split("_")[0])
-	cirCle_E=int(x.split("_")[1])
-	cirCle_D=int(x.split("_")[-1])
-	if cirCle_S <150 and cirCle_E>7139-150 and cirCle_D<-100:
-		return "1LTR_FL"
-	elif cirCle_S <150 and cirCle_E>7139-150 and cirCle_D>-100:
-		return "2LTR_FL"
-	elif cirCle_S <150 or cirCle_E>7139-150:
-		return "1LTR_Frg"
-	else:
-		return "nonLTR_Frg"
-
-def circleInfor(x,ref_len):
-	cirCle_S=int(x.split("_")[0])
-	cirCle_E=int(x.split("_")[1])
-	cirCle_D=int(x.split("_")[-1])
-	
-	if cirCle_S <100 and cirCle_E>ref_len and cirCle_D<-100:
-		return "E_E_%so"%(cirCle_D)
-	
-	
-		
-
 
 def getFile(r,Out_name):
 	f=pd.read_table(r)
 	f=f.loc[f["Circle"]!="NC"]
-	f=f.drop_duplicates(["Readname"],keep="first")
-	f["Type"]=f["Circle"].apply(lambda x : circleType(x))
+	f["D"]=f["Circle"].apply(lambda x: int(x.split("_")[-1]))
+	f.loc[f["D"]>1,"JunFeature"]="Ins"
+	f.loc[f["D"]<1."JunFeature"]="Del"
+	f.loc[f["D"]==1,"JunFeature"]="Adj"
+	f["CirS"]=f["Circle"].apply(lambda x: int(x.split("_")[0]))
+	f["CirE"]=f["Circle"].apply(lambda x: int(x.split("_")[1]))
+	f["CirSize"]=f["CirE"]-f["CirS"]+1
+	f=f.drop(["m","infor","p","D","CirS","CirE"],axis=1)
+	f.to_csv(Out_name,index=None,sep="\t",header=None)
 	
-	d=dict(zip(list(f["Readname"]),list(f["Type"])))
-
-	f_=pd.read_table(r)
-	f_=f_.loc[f_["Circle"]!="NC"]
-	f_["Type"]=f_["Readname"].apply(lambda x: d[x])
-	f_.to_csv(Out_name+"_data.txt",index=None,sep="\t")
-	g=f.groupby(["Type"],as_index=False).count()
-	g=g[["Type","Readname"]]
-	print(g)
-	g.to_csv(Out_name,index=None,sep="\t",header=None)
+	g=g.drop_duplicates(["Readname"],keep="first")
+	print("Number of circle reads: %s"%(g.shape[0]))
+	print("Largest circle size: %s"%(g["CirSize"].max()))
+	print("Smallest circle size: %s"%(g["CirSize"].min()))
+	print("Average circle size: %s"%(g["CirSize"].mean()))
 
 getFile(res1,name)
 
