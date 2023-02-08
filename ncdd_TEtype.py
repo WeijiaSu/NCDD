26a89386bda52e04cf6beb8445ced342f7544547
weijiasu <weijiasu0804@gmail.com>
2023-02-08 12:18:28 -0500

    comment

--- ncdd_TEtype.py
+++ ncdd_TEtype.py
@@ -6,47 +6,39 @@ import sys
 import pandas as pd
 pd.set_option("display.max_columns",40)
 
-
+# Get file name as an argument
 res1=sys.argv[1]
-name=sys.argv[2]
 
-def circleType(x):
-	cirCle_S=int(x.split("_")[0])
-	cirCle_E=int(x.split("_")[1])
-	cirCle_D=int(x.split("_")[2])
-	cirCle_L=int(x.split("_")[-1])
-	if cirCle_S <100 and cirCle_E>cirCle_L-100 and cirCle_D<=-100:
-		return "1End_FL"
-	elif cirCle_S <100 and cirCle_E>cirCle_L-100 and cirCle_D>-100:
-		return "2Ends_FL"
-	elif cirCle_S <100 or cirCle_E>cirCle_L-100:
-		return "1End_Frg"
-	else:
-		return "noEnd_Frg"
-		
-
-
-def getFile(r,Out_name):
-	f=pd.read_table(r)
-	f=f.loc[f["Circle"]!="NC"]
-	f=f.drop(["m","infor","p"],axis=1)
-	f["circileInfor"]=f["Circle"]+"_"+f["Reflen"].apply(str)
-	f=f.drop_duplicates(["Readname"],keep="first")
-	
-	f["Type"]=f["Circle"].apply(lambda x : circleType(x))
-	
-	d=dict(zip(list(f["Readname"]),list(f["Type"])))
-
-	f_=pd.read_table(r)
-	f_=f_.loc[f_["Circle"]!="NC"]
-	f_["Type"]=f_["Readname"].apply(lambda x: d[x])
-	f_.to_csv(Out_name+"_data.txt",index=None,sep="\t")
-	g=f.groupby(["Type"],as_index=False).count()
-	g=g[["Type","Readname"]]
-	print(g)
-	g.to_csv(Out_name,index=None,sep="\t",header=None)
-
-getFile(res1,name)
+def getFile(r):
+    # Read the input file
+    f=pd.read_table(r)
+    # Filter the rows where the "Circle" column has a value of "NC"
+    f=f.loc[f["Circle"]!="NC"]
+    # Add a new column "D" to the data frame which is the last part of the "Circle" column value split by "_"
+    f["D"]=f["Circle"].apply(lambda x: int(x.split("_")[-1]))
+    # Add a new column "JunFeature" to the data frame based on the value of the "D" column
+    f.loc[f["D"]>1,"JunFeature"]="Ins"
+    f.loc[f["D"]<1,"JunFeature"]="Del"
+    f.loc[f["D"]==1,"JunFeature"]="Adj"
+    # Add new columns "CirS" and "CirE" to the data frame which are the first and second part of the "Circle" column value split by "_"
+    f["CirS"]=f["Circle"].apply(lambda x: int(x.split("_")[0]))
+    f["CirE"]=f["Circle"].apply(lambda x: int(x.split("_")[1]))
+    # Add a new column "CirSize" to the data frame which is the difference between "CirE" and "CirS" plus 1
+    f["CirSize"]=f["CirE"]-f["CirS"]+1
+    # Remove the columns "m", "infor", "p", "D", "CirS", "CirE" from the data frame
+    f=f.drop(["m","infor","p","D","CirS","CirE"],axis=1)
+    # Write the modified data frame to a new file with a specified format
+    f.to_csv(r+".datatype.tsv",index=None,sep="\t")
+    # Filter the data frame to only include rows where the "Readname" column has more than 2 unique values
+    m=f.groupby(["Readname"],as_index=False).filter(lambda x:len(x)>2)    
+    # Remove duplicate rows based on the "Readname" column and keep only the first one
+    m=m.drop_duplicates(["Readname"],keep="first")
+    # Remove duplicate rows based on the "Readname" column and keep only the first one
+    g=f.drop_duplicates(["Readname"],keep="first")
+    # Print out various statistics about the filtered data frame
+    print("Number of circle reads: %s"%(g.shape[0]))
+	print("Largest circle size: %s"%(g["CirSize"].max()))
+    print("Small
 
 
 
