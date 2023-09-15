e2862004e031928d3d3d60f26b3d8a16b5192fb1
weijiasu <weijiasu0804@gmail.com>
2023-09-15 12:36:39 -0400

    update

--- NCDD.py
+++ NCDD.py
@@ -64,8 +64,8 @@ def filterFullreads(mapped_paf):
 def LinearReads(File):
     f=pd.read_table(File,header=0,sep="\t")
     f["RName"]=f["RName"].apply(str)
-    LinearAlignment=f.loc[(f["QStart"]<=100) & (f["QEnd"]>=f["QLen"]-100)]
-    LinearAlignment=LinearAlignment.groupby("QName").filter(lambda x: len(x)==1)
+    LinearAlignment=f.loc[(f["QStart"]<=fl) & (f["QEnd"]>=f["QLen"]-fl)]
+    #LinearAlignment=LinearAlignment.groupby("QName").filter(lambda x: len(x)==1)
     LinearAlignment.to_csv(pName+"_LiAg.tsv",index=None,sep="\t")
     candidateReads=f.loc[f["QName"].apply(lambda x: x not in list(LinearAlignment["QName"]))]
     candidateReads.to_csv(pName+".candi.tsv",index=None,sep="\t")
@@ -75,50 +75,50 @@ def LinearReads(File):
     
     print("Fully aligned reads %s: Linear Alignment %s (%s); Chimeric Alignment %s (%s)")%(s1,s2,round(s2/s1,2),s3.round(s3/s1,2))
 
-LinearReads(pName+".filter.full.paf")
-#
-#def Junction(list1,list2):
-#    n1,n2,n3,n4=list1[0],list1[1],list2[0],list2[1]
-#    if n3>n2 and n3-n2>100:
-#        return False
-#    elif abs(n3-n1)<100 or abs(n2-n4)<100:
-#        return False
-#    else:
-#        return True
-#
-#
-#def isCircle(list1,list2):
-#
-#    n1,n2,n3,n4=list1[0],list1[1],list2[0],list2[1]
-#    t1,t2,t3,t4=list1[2],list1[3],list2[2],list2[3]
-#    n_max=max(n1,n2,n3,n4)
-#    n_min=min(n1,n2,n3,n4)
-#    d1,d2=list1[4],list2[4]
-#    t_min=min(t1,t2,t3,t4)
-#    t_max=max(t1,t2,t3,t4)
-#    j=Junction(list1,list2)
-#    readlength=list1[-1]
-#    if d1!=d2 or j==False:
-#        return False
-#    else:
-#        if d1=="+" and t_max==t2:
-#            return True
-#        if d1=="-" and t_max==t4:
-#            return True
-#        else:
-#            return False
-#
-#def CompleteCopy(sub,coordinate):
-#    sub["s"]=sub["RefStart"].apply(lambda x: x>=int(coordinate[0])-100 and x<=int(coordinate[0])+100)
-#    sub["e"]=sub["RefEnd"].apply(lambda x: x>=int(coordinate[1])-100 and x<=int(coordinate[1])+100)
-#    sub=sub.loc[(sub["s"]==True) & (sub["e"]==True)]
-#    if sub.shape[0]>0:
-#        return True
-#    else:
-#        return False
-#	
-#
-#
+#LinearReads(pName+".filter.full.paf")
+
+def ReadConfigure(list1,list2):
+    n1,n2,n3,n4=list1[0],list1[1],list2[0],list2[1]
+    if n3>n2 and n3-n2>100:
+        return False
+    elif abs(n3-n1)<100 or abs(n2-n4)<100:
+        return False
+    else:
+        return True
+
+
+def isCircle(list1,list2):
+
+    n1,n2,n3,n4=list1[0],list1[1],list2[0],list2[1]
+    t1,t2,t3,t4=list1[2],list1[3],list2[2],list2[3]
+    n_max=max(n1,n2,n3,n4)
+    n_min=min(n1,n2,n3,n4)
+    d1,d2=list1[4],list2[4]
+    t_min=min(t1,t2,t3,t4)
+    t_max=max(t1,t2,t3,t4)
+    j=ReadConfigure(list1,list2)
+    readlength=list1[-1]
+    if d1!=d2 or j==False:
+        return False
+    else:
+        if d1=="+" and t_max==t2 and t_min==t3:
+            return True
+        if d1=="-" and t_max==t4 and t_min==t1:
+            return True
+        else:
+            return False
+
+def CompleteCopy(sub,coordinate):
+    sub["s"]=sub["RefStart"].apply(lambda x: x>=int(coordinate[0])-100 and x<=int(coordinate[0])+100)
+    sub["e"]=sub["RefEnd"].apply(lambda x: x>=int(coordinate[1])-100 and x<=int(coordinate[1])+100)
+    sub=sub.loc[(sub["s"]==True) & (sub["e"]==True)]
+    if sub.shape[0]>0:
+        return True
+    else:
+        return False
+	
+
+
 #def CircleType(list1,list2):
 #
 #    n1,n2,n3,n4=list1[0],list1[1],list2[0],list2[1]
@@ -136,49 +136,49 @@ LinearReads(pName+".filter.full.paf")
 #        return str(t_min)+"_"+str(t_max)+"_"+str(Overlap)
 #
 #
-#def getCircle(f):
-#    d={}
-#    infors=list(set(list(f["infor"])))
-#    for infor in infors:
-#        d[infor]="NC"
-#        sub=f.loc[f["infor"]==infor]
-#        sub=sub.sort_values(["Refname","ReadStart","ReadEnd"])
-#        l=list(zip(sub["ReadStart"],sub["ReadEnd"],sub["RefStart"],sub["RefEnd"],sub["Strand"],sub["Reflen"],sub["ReadLen"]))
-#        i=0
-#        while i<len(l)-1:
-#            list1=l[i]
-#            j=i+1
-#            list2=l[i+1]
-#            cirType=CircleType(list1,list2)
-#            if cirType!="NC":
-#                if sub.shape[0]==2:
-#                    d[infor]=cirType
-#                    break
-#                else:
-#                    coordinate=cirType.split("_")
-#                    comp=CompleteCopy(sub,coordinate)
-#                    if comp==True:
-#                        d[infor]=cirType
-#                        break
-#                    else:
-#                        i+=1
-#            else:
-#                i+=1
-#    f["Circle"]=f["infor"].apply(lambda x: d[x])
-#    return f
-#
-#
-#def GetReads(canfile):
-#    f_c=pd.read_table(canfile,header=0,sep="\t")
-#    f_c["infor"]=f_c["Readname"]+"_"+f_c["Refname"]
-#    f_c=f_c.sort_values(["infor","ReadStart","ReadEnd","RefStart","RefEnd"])
-#    f_circle=getCircle(f_c)
-#    f_circle.to_csv(canfile+"_circleAnalyze.txt",index=None,sep="\t")
-#    fc=f_circle.loc[f_circle["Circle"]!="NC"]
-#    fc.to_csv(pre+"_circles.txt",index=None,sep="\t")
-#    nc=f_circle.loc[f_circle["Circle"]=="NC"]
-#    circle=f_circle.drop_duplicates(["Readname"],keep="first").groupby(["Circle"],as_index=False).count()[["Circle","Readname"]].sort_values(["Circle"])
-#
+def getCircle(f):
+    d={}
+    infors=list(set(list(f["infor"])))
+    for infor in infors:
+        d[infor]="NC"
+        sub=f.loc[f["infor"]==infor]
+        sub=sub.sort_values(["Refname","ReadStart","ReadEnd"])
+        l=list(zip(sub["ReadStart"],sub["ReadEnd"],sub["RefStart"],sub["RefEnd"],sub["Strand"],sub["Reflen"],sub["ReadLen"]))
+        i=0
+        while i<len(l)-1:
+            list1=l[i]
+            j=i+1
+            list2=l[i+1]
+            cirType=CircleType(list1,list2)
+            if cirType!="NC":
+                if sub.shape[0]==2:
+                    d[infor]=cirType
+                    break
+                else:
+                    coordinate=cirType.split("_")
+                    comp=CompleteCopy(sub,coordinate)
+                    if comp==True:
+                        d[infor]=cirType
+                        break
+                    else:
+                        i+=1
+            else:
+                i+=1
+    f["Circle"]=f["infor"].apply(lambda x: d[x])
+    return f
+
+
+def GetReads(canfile):
+    f_c=pd.read_table(canfile,header=0,sep="\t")
+    f_c["infor"]=f_c["QName"]+"_"+f_c["RName"]
+    f_c=f_c.sort_values(["infor","QStart","QEnd","RStart","REnd"])
+    f_circle=getCircle(f_c)
+    f_circle.to_csv(canfile+"_circleAnalyze.txt",index=None,sep="\t")
+    fc=f_circle.loc[f_circle["Circle"]!="NC"]
+    fc.to_csv(pre+"_circles.txt",index=None,sep="\t")
+    nc=f_circle.loc[f_circle["Circle"]=="NC"]
+    circle=f_circle.drop_duplicates(["Readname"],keep="first").groupby(["Circle"],as_index=False).count()[["Circle","Readname"]].sort_values(["Circle"])
+
 #files=os.listdir("./")
 #if pre+"_rc90.tsv" not in files:
 #    print("Geting full read")
